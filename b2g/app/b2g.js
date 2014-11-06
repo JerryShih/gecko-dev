14464fe6397772771591b594a61ed7b2843d4b27
JerryShih <hshih@mozilla.com>
2014-11-06 17:48:07 +0800

    [silk] enable silk and vsync-aligned RD and compositor pref

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -30,6 +30,11 @@ pref("browser.sessionstore.resume_from_crash", false);
 pref("browser.tabs.remote.autostart.1", false);
 #endif
 
+pref("gfx.vsync", true);
+pref("gfx.vsync.hw-vsync.enabled", true);
+pref("gfx.vsync.compositor", true);
+pref("gfx.vsync.refresh-driver", true);
+
 // Bug 945235: Prevent all bars to be considered visible:
 pref("toolkit.defaultChromeFeatures", "chrome,dialog=no,close,resizable,scrollbars,extrachrome");
 
