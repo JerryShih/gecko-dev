720d71dff3e063c07c4d0f147be29b8843574468
JerryShih <hshih@mozilla.com>
2015-01-24 13:41:54 +0800

    Bug 1125030 - Handle RefreshDriverVsyncObserver shutdown in VsyncChild ActorDestroy()

--- layout/ipc/VsyncChild.cpp
+++ layout/ipc/VsyncChild.cpp
@@ -13,6 +13,7 @@ namespace layout {
 
 VsyncChild::VsyncChild()
   : mObservingVsync(false)
+  , mIsShutdown(false)
 {
   MOZ_ASSERT(NS_IsMainThread());
 }
@@ -26,9 +27,9 @@ bool
 VsyncChild::SendObserve()
 {
   MOZ_ASSERT(NS_IsMainThread());
-  if (!mObservingVsync) {
-    PVsyncChild::SendObserve();
+  if (!mObservingVsync && !mIsShutdown) {
     mObservingVsync = true;
+    PVsyncChild::SendObserve();
   }
   return true;
 }
@@ -37,9 +38,9 @@ bool
 VsyncChild::SendUnobserve()
 {
   MOZ_ASSERT(NS_IsMainThread());
-  if (mObservingVsync) {
-    PVsyncChild::SendUnobserve();
+  if (mObservingVsync && !mIsShutdown) {
     mObservingVsync = false;
+    PVsyncChild::SendUnobserve();
   }
   return true;
 }
@@ -48,13 +49,18 @@ void
 VsyncChild::ActorDestroy(ActorDestroyReason aActorDestroyReason)
 {
   MOZ_ASSERT(NS_IsMainThread());
-  mObserver = nullptr;
+  // Set shutdown flag first, and observer can't use SendObserve() or
+  // SendUnobserve() anymore.
+  mIsShutdown = true;
+  if (mObserver) {
+    mObserver->Shutdown();
+    mObserver = nullptr;
+  }
 }
 
 bool
 VsyncChild::RecvNotify(const TimeStamp& aVsyncTimestamp)
-{
-  MOZ_ASSERT(NS_IsMainThread());
+{  MOZ_ASSERT(NS_IsMainThread());
   if (mObservingVsync && mObserver) {
     mObserver->NotifyVsync(aVsyncTimestamp);
   }
