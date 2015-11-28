e9b168843cba789492df305aec357c7b8c554617
JerryShih <hshih@mozilla.com>
2015-11-28 21:08:00 +0800

    turn on off-main and layer transaction deferring

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -19,6 +19,9 @@
  *  - Computed values (e.g. 50 * 1024) don't work.
  */
 
+pref("gfx.content.off-main.painting", true);
+pref("gfx.content.layerTransaction.deferring", true);
+
 pref("keyword.enabled", false);
 pref("general.useragent.locale", "chrome://global/locale/intl.properties");
 pref("general.useragent.compatMode.firefox", false);
