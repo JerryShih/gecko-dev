58a21a0329c3563c9c1c0c441fd5fce9d0d47d87
JerryShih <hshih@mozilla.com>
2015-11-28 21:08:00 +0800

    Bug 1166173 - turn on off-main painting pref

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -19,6 +19,8 @@
  *  - Computed values (e.g. 50 * 1024) don't work.
  */
 
+pref("gfx.content.off-main.painting", true);
+
 pref("keyword.enabled", false);
 pref("general.useragent.locale", "chrome://global/locale/intl.properties");
 pref("general.useragent.compatMode.firefox", false);
