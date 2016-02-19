7d25b40965c23d9ca755e8544a9f247f07586ee3
JerryShih <hshih@mozilla.com>
2016-02-19 10:41:42 +0800

    Debug log: profiler label for clientLayerManager

--- gfx/layers/client/ClientLayerManager.cpp
+++ gfx/layers/client/ClientLayerManager.cpp
@@ -192,6 +192,8 @@ ClientLayerManager::CreateReadbackLayer()
 void
 ClientLayerManager::BeginTransactionWithTarget(gfxContext* aTarget)
 {
+  printf_stderr("bignose ClientLayerManager::BeginTransactionWithTarget, lm:%p", this);
+
   mInTransaction = true;
   mTransactionStart = TimeStamp::Now();
 
@@ -312,7 +314,12 @@ ClientLayerManager::EndTransactionInternal(DrawPaintedLayerCallback aCallback,
   }
 #endif
 
-  root->RenderLayer();
+  {
+    PROFILER_LABEL("ClientLayerManager", "EndTransactionInternal-RenderLayer",
+      js::ProfileEntry::Category::GRAPHICS);
+
+    root->RenderLayer();
+  }
 
 #ifdef MOZ_OFF_MAIN_PAINTING
   if (offMainPainting) {
@@ -359,6 +366,9 @@ ClientLayerManager::EndTransaction(DrawPaintedLayerCallback aCallback,
                                    void* aCallbackData,
                                    EndTransactionFlags aFlags)
 {
+  PROFILER_LABEL("ClientLayerManager", "EndTransaction",
+    js::ProfileEntry::Category::GRAPHICS);
+
   if (mWidget) {
     mWidget->PrepareWindowEffects();
   }
@@ -366,6 +376,9 @@ ClientLayerManager::EndTransaction(DrawPaintedLayerCallback aCallback,
   ForwardTransaction(!(aFlags & END_NO_REMOTE_COMPOSITE));
 
   if (mRepeatTransaction) {
+    PROFILER_LABEL("ClientLayerManager", "EndTransaction-RepeatTransaction",
+      js::ProfileEntry::Category::GRAPHICS);
+
     mRepeatTransaction = false;
     mIsRepeatTransaction = true;
     BeginTransaction();
@@ -382,6 +395,9 @@ ClientLayerManager::EndTransaction(DrawPaintedLayerCallback aCallback,
 bool
 ClientLayerManager::EndEmptyTransaction(EndTransactionFlags aFlags)
 {
+  PROFILER_LABEL("ClientLayerManager", "EndEmptyTransaction",
+    js::ProfileEntry::Category::GRAPHICS);
+
   mInTransaction = false;
 
   if (!mRoot) {
@@ -633,6 +649,8 @@ ClientLayerManager::StopFrameTimeRecording(uint32_t         aStartIndex,
 void
 ClientLayerManager::ForwardTransaction(bool aScheduleComposite)
 {
+  printf_stderr("bignose ClientLayerManager::ForwardTransaction, lm:%p", this);
+
   TimeStamp start = TimeStamp::Now();
 
   if (mForwarder->GetSyncObject()) {
