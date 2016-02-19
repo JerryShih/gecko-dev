bce346e194c8ade30b4e72d2ae44744dfadd22a3
JerryShih <hshih@mozilla.com>
2016-02-19 10:32:32 +0800

    Bug 1166173 - Create DrawTargetAsync for tiled drawTarget.

--- gfx/2d/Factory.cpp
+++ gfx/2d/Factory.cpp
@@ -45,6 +45,11 @@
 #include "HelpersD2D.h"
 #endif
 
+#ifdef MOZ_OFF_MAIN_PAINTING
+#include "AsyncPaintData.h"
+#include "DrawTargetAsync.h"
+#endif
+
 #include "DrawTargetDual.h"
 #include "DrawTargetTiled.h"
 #include "DrawTargetRecording.h"
@@ -158,6 +163,12 @@ namespace gfx {
 
 // In Gecko, this value is managed by gfx.logging.level in gfxPrefs.
 int32_t LoggingPrefs::sGfxLogLevel = LOG_DEFAULT;
+
+#ifdef MOZ_OFF_MAIN_PAINTING
+bool sOffMainPainting =
+  PreferenceAccess::RegisterLivePref("gfx.content.off-main.painting", &sOffMainPainting,
+                                     false);
+#endif
 
 #ifdef WIN32
 ID3D11Device *Factory::mD3D11Device;
@@ -445,11 +456,27 @@ Factory::CreateTiledDrawTarget(const TileSet& aTileSet)
 {
   RefPtr<DrawTargetTiled> dt = new DrawTargetTiled();
 
+#ifndef MOZ_OFF_MAIN_PAINTING
   if (!dt->Init(aTileSet)) {
     return nullptr;
   }
 
   return dt.forget();
+#else
+  if (sOffMainPainting) {
+    RefPtr<DrawTargetTiledAsync> drawTargetAsync = new DrawTargetTiledAsync(new DrawTargetAsyncPaintData(dt.get()));
+
+    if (drawTargetAsync->Init(aTileSet)) {
+      return drawTargetAsync.forget();
+    } else if (dt->Init(aTileSet)){
+      return dt.forget();
+    }
+  } else if (dt->Init(aTileSet)) {
+    return dt.forget();
+  }
+
+  return nullptr;
+#endif
 }
 
 bool
