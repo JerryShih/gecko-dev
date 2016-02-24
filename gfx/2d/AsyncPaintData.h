4bdd3d83311ad0263adc74d05c0bd6232cc0ca93
JerryShih <hshih@mozilla.com>
2016-02-24 16:36:10 +0800

    Bug 1166173 - AsyncPaintData for draw command recording

---
+++ gfx/2d/AsyncPaintData.h
@@ -0,0 +1,73 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef MOZILLA_GFX_ASYNCPAINTDATA_H_
+#define MOZILLA_GFX_ASYNCPAINTDATA_H_
+
+#include <vector>
+
+#include "IterableArena.h"
+#include "mozilla/RefCounted.h"
+#include "mozilla/RefPtr.h"
+#include "mozilla/UniquePtr.h"
+
+namespace mozilla {
+namespace gfx {
+
+class DrawTarget;
+class DrawTargetAsync;
+class DrawingCommand;
+
+// Used for recording drawTarget draw command and replaying.
+class AsyncPaintData : public external::AtomicRefCounted<AsyncPaintData>
+{
+  friend class DrawTargetAsync;
+
+public:
+  MOZ_DECLARE_REFCOUNTED_VIRTUAL_TYPENAME(AsyncPaintData);
+
+  AsyncPaintData();
+  virtual ~AsyncPaintData();
+
+  template<typename CommandType, typename... Args>
+  void AppendDrawCommand(Args&&... aArgs)
+  {
+    static_assert(IsBaseOf<DrawingCommand, CommandType>::value,
+                  "CommandType must derive from DrawingCommand");
+
+    InitPool();
+
+    ptrdiff_t index = mCommandPool->Alloc<CommandType>(Forward<Args>(aArgs)...);
+    mPendingDrawCommandIndex.push_back(index);
+  }
+
+  // Return nullptr if there is no command in buffer.
+  DrawingCommand* GetLastCommand();
+
+  void ApplyPendingDrawCommand();
+
+  // Get current ref drawTarget. Should be called at main thread.
+  virtual DrawTarget* GetDrawTarget() = 0;
+
+  // Prepare the drawTarget for GetDrawTargetForAsyncPainting().
+  virtual void LockForAsyncPainting() = 0;
+  virtual void UnlockForAsyncPainting() = 0;
+  // Get current ref drawTarget.
+  virtual DrawTarget* GetDrawTargetForAsyncPainting() = 0;
+
+private:
+  void InitPool();
+  void ClearPoolDrawCommand();
+  void ReleasePool();
+
+  std::vector<ptrdiff_t> mPendingDrawCommandIndex;
+  UniquePtr<IterableArena> mCommandPool;
+
+  bool mIsPoolReady;
+};
+
+} //namespace gfx
+} //namespace mozilla
+
+#endif  //MOZILLA_GFX_ASYNCPAINTDATA_H_
