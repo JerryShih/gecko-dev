9776235c54102948eb1c6a64dea9fd87ac17d253
JerryShih <hshih@mozilla.com>
2016-03-11 16:12:08 +0800

    Bug 1235018 - messageChannel deferring mode implementation

--- ipc/glue/MessageChannel.h
+++ ipc/glue/MessageChannel.h
@@ -206,6 +206,10 @@ class MessageChannel : HasResultCodes
         sIsPumpingMessages = aIsPumping;
     }
 
+    // These functions can be used at any thread.
+    bool StartDeferring();
+    bool EndDeferring();
+
 #ifdef OS_WIN
     struct MOZ_STACK_CLASS SyncStackFrame
     {
@@ -433,6 +437,10 @@ class MessageChannel : HasResultCodes
     bool ShouldDeferMessage(const Message& aMsg);
     void OnMessageReceivedFromLink(Message&& aMsg);
     void OnChannelErrorFromLink();
+
+    // Return true if |aMsg| is a special message that turn on/off ipc deferring
+    // mode.
+    bool MaybeInterceptSpecialDeferMessage(const Message& aMsg);
 
   private:
     // Run on the not current thread.
@@ -791,6 +799,17 @@ class MessageChannel : HasResultCodes
     RefPtr<RefCountedTask> mOnChannelConnectedTask;
     bool mPeerPidSet;
     int32_t mPeerPid;
+
+    // If the child side turns on the deferring mode, all messages(including sync,
+    // async and intr) will be pushed into mIPCDeferringMessage. Then, be processed
+    // when the child side exit deferring mode.
+    MessageQueue mIPCDeferringMessage;
+
+    // Used at parent side.
+    bool mDeferring;
+
+    // Used at child side.
+    DebugOnly<bool> mOtherSideDeferring;
 };
 
 void
