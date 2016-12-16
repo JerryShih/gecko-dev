054c22fdcd7783ade65c6334f31208e330dab7de
JerryShih <hshih@mozilla.com>
2016-12-16 16:35:31 +0800

    Handle the releasing of external images in api.delete_image().
    Merge ResultMsg::UpdateTextureData and NewFrame::NewFrame message into one message.
    Release the external images when a new frame is ready.
    
    MozReview-Commit-ID: 6rYRe8WOaAN

--- gfx/webrender/src/internal_types.rs
+++ gfx/webrender/src/internal_types.rs
@@ -351,6 +351,8 @@ pub enum TextureUpdateOp {
     Grow(u32, u32, ImageFormat, TextureFilter, RenderTargetMode),
 }
 
+pub type ExternalImageUpdateList = Vec<ExternalImageId>;
+
 pub struct TextureUpdate {
     pub id: CacheTextureId,
     pub op: TextureUpdateOp,
@@ -399,9 +401,8 @@ impl RendererFrame {
 }
 
 pub enum ResultMsg {
-    UpdateTextureCache(TextureUpdateList),
     RefreshShader(PathBuf),
-    NewFrame(RendererFrame, BackendProfileCounters),
+    NewFrame(RendererFrame, TextureUpdateList, ExternalImageUpdateList, BackendProfileCounters),
 }
 
 #[repr(u32)]
